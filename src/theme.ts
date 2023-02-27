4bbf5c83807c46676f4403a7e3b815a60ed446ef
조정훈 <jeongh1021@naver.com>
2023-02-27 17:11:30 +0900

    to-do App init

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#1F2937",
+  itemBgColor: "#374151",
+  textColor: "white",
+  accentColor: "#3B81F6",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#efefef",
+  itemBgColor: "white",
+  textColor: "#222222",
+  accentColor: "#3B81F6",
+};
