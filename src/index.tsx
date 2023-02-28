e647f39581438e8e1d585667a9b707290029a3a4
조정훈 <jeongh1021@naver.com>
2023-02-28 23:30:32 +0900

    라우터관련 병경

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ import { RecoilRoot } from "recoil";
 import App from "./App";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <ChakraProvider>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <App />
-      </ChakraProvider>
+      <HashRouter>
+        <ChakraProvider>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <App />
+        </ChakraProvider>
+      </HashRouter>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
